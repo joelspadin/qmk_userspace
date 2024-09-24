fcd28f45000c0cfeea31b42374cc55aabe1016f3
Joel Spadin <joelspadin@gmail.com>
2024-09-23 19:48:47 -0500

    Initial commit

---
+++ keyboards/keebsforall/freebirdtkl/keymaps/default/config.h
@@ -0,0 +1,7 @@
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+
+// Yay, hot swap sockets
+#define DEBOUNCE 40
+
+// Debug stuff.
+// #define DEBUG_MATRIX_SCAN_RATE
---
+++ keyboards/keebsforall/freebirdtkl/keymaps/default/rules.mk
@@ -0,0 +1,9 @@
+CAPS_WORD_ENABLE = yes
+DEBOUNCE_TYPE = asym_eager_defer_pk
+EXTRAKEY_ENABLE = yes
+NKRO_ENABLE = yes
+VIA_ENABLE = yes
+
+# Debug stuff.
+# CONSOLE_ENABLE = yes
+# COMMAND_ENABLE = yes
